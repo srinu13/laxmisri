82a9ed1b5d63c9f5ba2a64963fcd2f81024053ec
srinu13 <39088493+srinu13@users.noreply.github.com>
2021-02-06 22:43:55 +0530

    Add files via upload
    Gateservice

---
+++ ServiceRegistry/src/test/java/com/ubi/ServiceRegistryTests.java
@@ -0,0 +1,16 @@
+package com.ubi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceRegistryTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ gateway-service/src/main/java/com/example/demo/GatewayServiceApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	public AlwaysSampler defaultSampler() {
+		return new AlwaysSampler();
+	}
+}
